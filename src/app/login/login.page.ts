9aa17eb0ed4da2c776a03276e5394824d1db939e
whoizgil <vapas2005@gmail.com>
2024-05-01 18:39:13 -0300

    login feito

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,19 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage  {
+  email: string = '';
+  password: string = '';
   showPassword: boolean = false;
 
-  constructor() { }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private toastController: ToastController,
+    private router: Router
+  ) { }
 
-  ngOnInit() {
-  }
 
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
+  }
+
+  async login() {
+    console.log("E-mail:", this.email);
+    if (!this.isValidEmail(this.email)) {
+      this.presentErrorToast('Por favor, insira um endereço de e-mail válido');
+      return;
+    }
+
+    try {
+      const userCredential = await this.afAuth.signInWithEmailAndPassword(this.email, this.password);
+      this.presentSuccessToast('Usuário logado com sucesso');
+      this.router.navigate(['/home']);
+    } catch (error) {
+      console.error('Erro ao autenticar:', error);
+      this.presentErrorToast('E-mail ou Senha inválidos');
+    }
+  }
+
+  isValidEmail(email: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  async presentSuccessToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      color: 'success'
+    });
+    toast.present();
+  }
+
+  async presentErrorToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      color: 'danger'
+    });
+    toast.present();
   }
 }
