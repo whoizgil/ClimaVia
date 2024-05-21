9407ba69ccc7491a4a9caf79ac10c851886c022f
whoizgil <vapas2005@gmail.com>
2024-05-21 14:59:46 -0300

    git teste

---
+++ android/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ClimaVia',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
