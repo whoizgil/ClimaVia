84acfa06761823245cea7434e2e002850de97d11
whoizgil <vapas2005@gmail.com>
2024-05-27 22:14:43 -0300

    erro environment RESOLVIDO + ionic buildado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { PesquisaPage } from './pesquisa/pesquisa.page'; // Importe a página de pesquisa
 
 const routes: Routes = [
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,28 @@ import { HttpClientModule } from '@angular/common/http';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import {firebaseConfig} from '../../environments/environment';
+
+import { environment } from '../../environments/environment'; 
 import { AngularFireModule } from '@angular/fire/compat';
 import { FormsModule } from '@angular/forms';
 
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AngularFireModule.initializeApp(firebaseConfig),BrowserModule, FormsModule, AngularFireAuthModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, provideFirebaseApp(() => initializeApp({"projectId":"climavia-7b803","appId":"1:321548348876:web:a82403d3991ed245010169","databaseURL":"https://climavia-7b803-default-rtdb.firebaseio.com","storageBucket":"climavia-7b803.appspot.com","apiKey":"AIzaSyBX2BtfIMLIChvUGu7A_mIe_V2IKCp-Nqg","authDomain":"climavia-7b803.firebaseapp.com","messagingSenderId":"321548348876","measurementId":"G-78WL52629Q"})), provideFirestore(() => getFirestore())],
+  imports: [
+    AngularFireModule.initializeApp(environment.firebaseConfig), 
+    BrowserModule,
+    FormsModule,
+    AngularFireAuthModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), 
+    provideFirestore(() => getFirestore())
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent]
 })
