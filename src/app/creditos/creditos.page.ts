6a8245847a47fffa67e8a283d83bf71c9decc041
Arthur <arthurbdefreitas@gmail.com>
2024-06-10 23:16:05 -0300

    tela de creditos iniciada

--- src/app/creditos/creditos.page.ts
+++ src/app/creditos/creditos.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-creditos',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreditosPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private alertController: AlertController 
+) { }
 
   ngOnInit() {
   }
+ 
+  async mostrarCaixaDuvida() {
+    const alert = await this.alertController.create({
+      cssClass: 'custom-alert',
+      message: 'Aqui você encontrará os criadores do Projeto/Aplicativo.',
+      buttons: ['OK']
+    });
 
+    await alert.present();
+  }
 }
